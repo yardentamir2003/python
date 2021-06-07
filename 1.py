115d135669fd48444b3dd06db23820ac4eb358b0
yarden <yardent9@gmail.com>
2021-06-07 13:47:21 +0300

    cotere

--- 1.py
+++ 1.py
@@ -5,3 +5,5 @@ def do_math(num1, num2):
 math1=do_math(5,7)
 math2=do_math(11,34)
 print("sum one is", math1, "and sum two is", math2)
+print("sum one is", math1, "and sum two is", math2)
+print("sum one is", math1, "and sum two is", math2)
