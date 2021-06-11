21a81370286d53352720476945655a5a326a4d2e
yarden <yardent9@gmail.com>
2021-06-11 15:28:34 +0300

    cotere

--- 3.py
+++ 3.py
@@ -1,15 +1,24 @@
 hours = input("Enter hours: ")
-rate = input("Enter rate: ")
 try:
     hours = float(hours)
-    rate = float(rate)
-    if hours < 40:
-        print("pay: ", float(hours) * float(rate))
-
-    else:
-        one = int(40) * float(rate)
-        two = float(hours - 40) * float(rate) * 1.5
-    print("pay: ", int(one + two))
-
 except:
-       print("Please enter numeric input")
+    print("Please enter numeric input")
+    exit(1)
+rate = input("Enter rate: ")
+try:
+    rate = float(rate)
+except:
+    print("Please enter numeric input")
+    exit(1)
+
+
+
+
+
+if hours < 40:
+    print("pay: ", float(hours) * float(rate))
+
+else:
+    one = int(40) * float(rate)
+    two = float(hours - 40) * float(rate) * 1.5
+    print("pay: ", int(one + two))
---
+++ 6.py
@@ -0,0 +1,26 @@
+
+score = input("Enter score: ")
+try:
+    score = float(score)
+except:
+    print("Bad score")
+    exit(1)
+
+if score > 1:
+    print("Bad score")
+    exit(1)
+
+if score < 0:
+    print("Bad score")
+    exit(1)
+
+if score >= 0.9:
+    print("A")
+elif score >= 0.8:
+    print("B")
+elif score >= 0.7:
+    print("C")
+elif score >= 0.6:
+    print("D")
+elif score < 0.6:
+    print("F")
