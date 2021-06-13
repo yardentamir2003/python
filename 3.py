ae7c9b8dc83bb042c0be5a49b7c177a10cb7c7d8
yarden <yardent9@gmail.com>
2021-06-13 15:33:29 +0300

    cotere

--- 3.py
+++ 3.py
@@ -19,6 +19,6 @@ if hours < 40:
     print("pay: ", float(hours) * float(rate))
 
 else:
-    one = int(40) * float(rate)
+    one = 40 * float(rate)
     two = float(hours - 40) * float(rate) * 1.5
-    print("pay: ", int(one + two))
+    print("pay: ", one + two)
