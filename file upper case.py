22b76fa2f5a6a5fcfa75a95531c544ca782b9803
yarden <yardent9@gmail.com>
2021-08-02 17:42:10 +0300

    cotere

---
+++ file upper case.py
@@ -0,0 +1,10 @@
+file_name = "C:\\Users\\yarde\\Downloads\\mbox-short.txt"
+file_name = "src/a.txt"
+
+file_name = input("Enter a file name: ")
+file = open(file_name)
+
+for line in file:
+    line = line.upper()
+    line = line.rstrip()
+    print(line)
