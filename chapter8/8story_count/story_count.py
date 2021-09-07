b1b05ca7a04b94b78a15776f4d8545d16e895678
yarden <yardent9@gmail.com>
2021-09-07 12:00:26 +0300

    cotere

---
+++ chapter8/8story_count/story_count.py
@@ -0,0 +1,35 @@
+def story_count(file_name):
+    word_list = []
+    count_list = []
+    file = open(file_name)
+    for line in file:
+        line = line.lower()
+        line = line.rstrip()
+        words = line.split(" ")
+        for word in words:
+            if word in word_list:
+                index = find_index(word, word_list)
+                count_list[index] = count_list[index] + 1
+
+            else:
+                word_list.append(word)
+                count_list.append(1)
+    return word_list,count_list
+
+
+def find_index(word, word_list):
+    index = 0
+    while index < len(word_list):
+        if word == word_list[index]:
+            break
+        else:
+            index = index + 1
+
+    return index
+
+
+file_name = input("Enter a file name: ")
+word_list, count_list = story_count(file_name)
+for i in range(len(word_list)):
+    print(word_list[i], end = " ")
+    print(count_list[i])
