0876052dbc56f4383ff6507d1e659665d1ae022e
yarden <yardent9@gmail.com>
2021-09-18 15:18:22 +0300

    cotere

--- chapter9/7word_position/word_position.py
+++ chapter9/7word_position/word_position.py
@@ -2,7 +2,7 @@ import re
 
 
 def main():
-    file_dictionary = dict()
+    file_dictionary = {}
     file = open("input.txt")
     line_number = 0
     for line in file:
@@ -12,24 +12,28 @@ def main():
         line = line.rstrip()
         parse_line(file_dictionary, line, line_number)
         line_number += 1
+    print_output(file_dictionary)
 
 
 def parse_line(file_dictionary, line, line_number):
     words = line.split(" ")
     word_number = 0
     for word in words:
-        if word not in file_dictionary:
-            val = "line {} word {}".format(line_number + 1, word_number + 1)
-            file_dictionary[word] = [val]
-        else:
-            word_number += 1
-            val = "line {} word {}".format(line_number + 1, word_number + 1)
-            file_dictionary[word].append(val)
+        val = "line {} word {}".format(line_number + 1, word_number + 1)
 
+        if word in file_dictionary:
+            file_dictionary[word].append(val)
+        else:
+            file_dictionary[word] = [val]
+
+        word_number += 1
+
+
+def print_output(file_dictionary):
     for word in file_dictionary:
         print(word, end=" : ")
-        values = file_dictionary[word]
-        for item in values:
+        location_list = file_dictionary[word]
+        for item in location_list:
             print(item, end=" ")
         print()
 
