1deacc0914ec180d1ef52f8ee7aa00f9f1ba3d5f
yarden <yardent9@gmail.com>
2021-09-24 19:59:05 +0300

    cotere

---
+++ chapter10/1most_emails/most_emails.py
@@ -0,0 +1,29 @@
+def main():
+    counts = dictionary_creator()
+    tuple_list(counts)
+
+
+def dictionary_creator():
+    file = open("input.txt")
+    counts = {}
+    for line in file:
+        if line.startswith("From "):
+            line = line.split(" ")
+            mail = line[1]
+            if mail in counts:
+                counts[mail] += 1
+            else:
+                counts[mail] = 1
+    return counts
+
+
+def tuple_list(counts):
+    lst = []
+    for key, val in counts.items():
+        lst.append((val, key))
+    lst.sort(reverse=True)
+    item = lst[0]
+    print(item[1], item[0])
+
+
+main()
