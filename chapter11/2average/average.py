9b14c48320c246bf914317a1942eeb16c6c588ab
yarden <yardent9@gmail.com>
2022-01-24 15:51:55 +0200

    cotere

---
+++ chapter11/2average/average.py
@@ -0,0 +1,18 @@
+import re
+
+
+def main():
+    file = open("input.txt")
+    sum = 0
+    count = 0
+    for line in file:
+        line = line.rstrip()
+        x = re.findall('^New Revision: ([0-9]+)', line)
+        if len(x) > 0:
+            x = int(x[0])
+            sum = sum + x
+            count += 1
+    avarage = int(sum/count)
+    print("Avarage:", avarage)
+
+main()
