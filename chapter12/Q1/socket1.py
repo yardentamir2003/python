8d3b35046135cb571aec16331d95ff5818b2e132
yarden <yardent9@gmail.com>
2022-03-11 16:14:30 +0200

    cotere

--- chapter12/Q1/socket1.py
+++ chapter12/Q1/socket1.py
@@ -3,19 +3,24 @@ import socket
 URL = input("Enter URL: ")
 splitted_URL = URL.split("/")
 host_name = splitted_URL[2]
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
-    mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     mysocket.connect((host_name, 80))
-    cmd = ("GET #{} HTTP/1.0\r\n\r\n".format(URL)).encode()
-    mysocket.send(cmd)
-
-    while True:
-        data = mysocket.recv(512)
-        if len(data) < 1:
-            break
-        print(data.decode(), end='')
-
-    mysocket.close()
-
 except:
     print("Non-existent URL.")
+
+
+cmd = ("GET #{} HTTP/1.0\r\n\r\n".format(URL)).encode()
+mysocket.send(cmd)
+
+while True:
+    data = mysocket.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end='')
+
+mysocket.close()
+
+
+
+
