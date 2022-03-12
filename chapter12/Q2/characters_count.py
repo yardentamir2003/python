438b6c30cf05ee9fcb5fa84f1540c9df563e7f2c
yarden <yardent9@gmail.com>
2022-03-12 18:06:51 +0200

    cotere

--- chapter12/Q2/characters_count.py
+++ chapter12/Q2/characters_count.py
@@ -1,9 +1,36 @@
+import socket
+
+URL = input("Enter URL: ")
+splitted_URL = URL.split("/")
+host_name = splitted_URL[2]
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    mysocket.connect((host_name, 80))
+except:
+    print("Non-existent URL.")
 
 
+cmd = ("GET {} HTTP/1.0\r\n\r\n".format(URL)).encode()
+mysocket.send(cmd)
 
-
+enter_here = True
+characters_count = 0
 while True:
-    data = mysocket.recv(3000)
-    if len(data) < 1:
+    limit = 999
+    data = mysocket.recv(100)
+    if len(data) == 0:
         break
-    print(data.decode(), end='')
+    characters_count += len(data)
+    if characters_count < 999:
+        print(data.decode(), end='')
+    else:
+        if enter_here:
+            dont_print = characters_count - limit
+            print(data[:-dont_print].decode(), end='')
+            enter_here = False
+print("\ncharacters count = ", characters_count)
+mysocket.close()
+
+
+
+
