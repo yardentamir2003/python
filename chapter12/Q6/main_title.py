4863ec8c947c718adb755ca0f27dd0b1aa140b98
yarden <yardent9@gmail.com>
2022-03-19 17:16:07 +0200

    cotere

---
+++ chapter12/Q6/main_title.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+url = "https://www.walla.co.il/"
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+# Retrieve all of the anchor tags
+tags = soup('h2')
+print(tags[0])
+
