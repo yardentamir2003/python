e568eb43973b17c6e5a68550b847fb573a759849
yarden <yardent9@gmail.com>
2022-03-26 21:51:43 +0300

    cotere

--- basics/num_in_words/main.py
+++ basics/num_in_words/main.py
@@ -1,10 +1,10 @@
-num2word = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five',
-            6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten',
-            11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen',
-            15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen',
-            19: 'Nineteen', 20: 'Twenty', 30: 'Thirty', 40: 'Forty',
-            50: 'Fifty', 60: 'Sixty', 70: 'Seventy', 80: 'Eighty',
-            90: 'Ninety', 0: 'Zero'}
+number2word = {0: 'Zero', 1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five',
+               6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten',
+               11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen',
+               15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen',
+               19: 'Nineteen', 20: 'Twenty', 30: 'Thirty', 40: 'Forty',
+               50: 'Fifty', 60: 'Sixty', 70: 'Seventy', 80: 'Eighty',
+               90: 'Ninety'}
 
 
 def main():
@@ -16,49 +16,51 @@ def main():
 def return_answer(number):
     try:
         number = int(number)
-        if number <= 20:
-            words = less_than_20(number)
-        elif 20 <= number <= 99:
-            words = two_digits(number)
-        elif 100 <= number <= 999:
-            words = three_digits(number)
-        elif 1000 <= number <= 9999:
-            words = four_digits(number)
-        else:
-            if number == 10000:
-                words = "Ten thousands"
-            else:
-                words = "Error please enter a number up to 10000."
     except:
         words = "Error please enter a number up to 10000."
+        return words
+
+    if number <= 20:
+        words = less_than_20(number)
+    elif 20 < number <= 99:
+        words = two_digits(number)
+    elif 100 <= number <= 999:
+        words = three_digits(number)
+    elif 1000 <= number <= 9999:
+        words = four_digits(number)
+    elif number == 10000:
+        words = "Ten thousands"
+    else:
+        words = "Error please enter a number up to 10000."
+
     return words
 
 
 def less_than_20(number):
-    word = num2word.get(number)
+    word = number2word.get(number)
     return word
 
 
 def two_digits(number):
     tens_digit = str(number)[0]
     tens = int(tens_digit) * 10
-    tens = num2word.get(int(tens))
+    tens = number2word.get(int(tens))
     unit_digit = str(number)[1]
-    word = num2word.get(int(unit_digit))
+    word = number2word.get(int(unit_digit))
     if word == "Zero" and tens == "Zero":
         return ""
-    elif unit_digit == "0":
+    if unit_digit == "0":
         return tens
-    elif tens == "Zero":
+    if tens == "Zero":
         return word
-    else:
-        answer = "{} {}".format(tens, word)
-        return answer
+
+    answer = "{} {}".format(tens, word)
+    return answer
 
 
 def three_digits(number):
     hundreds = str(number)[0]
-    hundreds = num2word.get(int(hundreds))
+    hundreds = number2word.get(int(hundreds))
     tens = str(number)[1:]
     tens = two_digits(tens)
     if hundreds == "Zero":
@@ -73,7 +75,7 @@ def three_digits(number):
 
 def four_digits(number):
     thousands = str(number)[0]
-    thousands = num2word.get(int(thousands))
+    thousands = number2word.get(int(thousands))
     hundreds = str(number)[1:]
     hundreds = three_digits(hundreds)
     if thousands == "One":
--- basics/num_in_words/test.py
+++ basics/num_in_words/test.py
@@ -18,23 +18,15 @@ def parse_line(line, line_number):
     line = line.strip()
     divided_line = line.split(",")
     expected_result = divided_line[1]
-    if expected_result == "Error please enter a number up to 10000.":
-        actual_result = return_answer(expected_result)
-        if expected_result != actual_result:
-            print("There's a problem with line number {}. should have been: {}, but got: {}".format(line_number,
-                                                                                                    expected_result,
-                                                                                                    actual_result))
-            return False
-    else:
-        number = int(divided_line[0])
-        actual_result = return_answer(number)
-        lower_actual_result = actual_result.lower()
-        lower_actual_result = lower_actual_result.strip()
-        if expected_result != lower_actual_result:
-            print("There's a problem with line number {}. should have been: {}, but got: {}".format(line_number,
-                                                                                                    expected_result,
-                                                                                                    lower_actual_result))
-            return False
+    number = divided_line[0]
+    actual_result = return_answer(number)
+    lower_actual_result = actual_result.lower()
+    lower_actual_result = lower_actual_result.strip()
+    if expected_result != lower_actual_result:
+        print("There's a problem with line number {}. should have been: {}, but got: {}".format(line_number,
+                                                                                                expected_result,
+                                                                                                lower_actual_result))
+        return False
     return True
 
 
