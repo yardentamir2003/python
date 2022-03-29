b8f122506210242d9cec050f74fff5161c058c43
Yarden <yardent9@gmail.com>
2022-03-29 21:25:21 +0300

    test

---
+++ basics/candidates_files/main.py
@@ -0,0 +1,11 @@
+import json
+
+
+def main():
+    files_amount = 0
+    print("Welcome candidate!\nPlease enter the following details:")
+    file_name = cadidate_XXX.json
+    json_file = open("cadidate_XXX.json")
+
+
+main()
