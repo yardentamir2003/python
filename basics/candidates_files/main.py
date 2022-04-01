096f5fb0e8d7bf821874783b805b6859b4b7ff5d
Yarden <yardent9@gmail.com>
2022-04-01 15:16:52 +0300

    test

--- basics/candidates_files/main.py
+++ basics/candidates_files/main.py
@@ -3,52 +3,65 @@ import re
 
 
 def main():
-    files_amount = 0
+    #files_amount = 0
     print("Welcome candidate!\nPlease enter the following details:")
-    file_name = cadidate_XXX.json
-    json_file = open("cadidate_XXX.json")
+    #file_name = cadidate_XXX.json
+    #json_file = open("cadidate_XXX.json")
 
     first_name = input("What is your first name: ")
     last_name = input("What is your last name: ")
     while True:
         birthday_date = input("What is your birth date: ")
-        x = check_date(birthday_date)
-        if x is None:
+        invalid_date = check_date(birthday_date)
+        if invalid_date is None:
             continue
+        if not invalid_date:
+            break
     birth_country = input("What is your birth country: ")
     if birth_country == "israel" or "Israel":
         israel_functuon()
     courrent_country = input("What is your current country: ")
     city =input("What is your current city: ")
     street = input("What is your current street address: ")
-    knows_candidats()
+    knows_candidates()
 
 
 def check_date(date):
-match_date = re.findall('^[0-9]{4}-[0-9]{2}-[0-9]{2}$', date)
-if len(match_date) == 0:
-    print("Ahhh.. Are you sure? Please enter date in format yyyy-mm-dd")
-    return None
-else:
-    return False
+    match_date = re.findall('^[0-9]{4}-[0-9]{2}-[0-9]{2}$', date)
+    if len(match_date) == 0:
+        print("Ahhh.. Are you sure? Please enter date in format yyyy-mm-dd")
+        return None
+    else:
+        return False
+
+
+def check_yes_no(question):
+    match_answer = re.findall('"yes"|"no"', question)
+    if len(match_answer) == 0:
+        print("Ahhh.. Are you sure? Please enter yes or no")
+        return None
+    else:
+        return False
 
 
 def israel_function():
     while True:
         israel_entrance = input("When did you enter to Israel: ")
-        x = check_date(israel_entrance)
-        if x is None:
+        invalid_date = check_date(israel_entrance)
+        if invalid_date is None:
             continue
-    hebrew_speaker = input("Do you speak hebrew (yes/no): ")
+        if not invalid_date:
+            break
     while True:
-        yes_no = re.findall('"yes"|"no"', hebrew_speaker)
-        if len(yes_no) == 0:
-            print("Ahhh.. Are you sure? Please enter yes or no")
-            return None
-        else:
+        hebrew_speaker = input("Do you speak hebrew (yes/no): ")
+        invalid_answer = check_yes_no(hebrew_speaker)
+        if invalid_answer is None:
+            continue
+        if not invalid_answer:
             return False
 
-def knows_candidats():
+
+def knows_candidates():
     know_candidates = input("Do you know any candidates (yes/no): ")
     while True:
         yes_no = re.findall('"yes"|"no"', know_candidates)
