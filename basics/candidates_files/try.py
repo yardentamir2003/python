70cb1df033dbb58f51aa5da1c783c52b24e2c368
Yarden <yardent9@gmail.com>
2022-04-02 15:13:07 +0300

    test

--- basics/candidates_files/try.py
+++ basics/candidates_files/try.py
@@ -2,37 +2,8 @@ import json
 import re
 
 
-def main():
-    print("Welcome candidate!\nPlease enter the following details:")
-    files_amount = 001
-    for file in files:
-        if files_amount > 100:
-            print("candidates list is full, try next year.")
-            exit(0)
-        file_name = "cadidate_{}.json".format(files_amount)
-        ask_questions()
-        files_amount += 1
-
-def build_dictionary():
-    d = {}
-    d["first name"] = first_name
-    d["last name"] = last_name
-    d["birth day"] = birth_date
-    d["parent_approval"] = parent_approval
-    d["grade"] = grade
-    d["school"] = school
-    d["birth country"] = birth_country
-    d["israel entrance"] = israel_entrance
-    d["hebrew speaker"] = hebrew_speaker
-    d["courrent country"] = courrent_country
-    d["city"] = city
-    d["street"] = street
-    d["familiar candudates"] = candidate_name
-
-
-
-
 def ask_questions():
+    d = {}
     first_name = input("What is your first name: ")
     last_name = input("What is your last name: ")
     while True:
@@ -45,11 +16,39 @@ def ask_questions():
     under_eighteen(birth_date)
     birth_country = input("What is your birth country: ")
     if birth_country != "israel" and birth_country != "Israel":
-        non_israel_function()
-    courrent_country = input("What is your current country: ")
+        israel_entrance, hebrew_speaker = non_israel_function()
+    current_country = input("What is your current country: ")
     city = input("What is your current city: ")
     street = input("What is your current street address: ")
-    knows_candidates()
+    candidates_names = knows_candidates()
+    d["first name"] = first_name
+    d["last name"] = last_name
+    d["birth day"] = birth_date
+    d["parent_approval"] = parent_approval
+    d["grade"] = grade
+    d["school"] = school
+    d["birth country"] = birth_country
+    d["israel entrance"] = israel_entrance
+    d["hebrew speaker"] = hebrew_speaker
+    d["courrent country"] = current_country
+    d["city"] = city
+    d["street"] = street
+    d["familiar candudates"] = candidates_names
+    return d
+
+
+def main():
+    print("Welcome candidate!\nPlease enter the following details:")
+    files_amount = 0o1
+    for file in files:
+        if files_amount > 100:
+            print("candidates list is full, try next year.")
+            exit(0)
+        file_name = "cadidate_{}.json".format(files_amount)
+        d = ask_questions()
+        output_file = open(file_name, "w")
+        json.dump(d, output_file, indent = 6)
+        files_amount += 1
 
 
 def check_date(date):
@@ -84,7 +83,7 @@ def non_israel_function():
         if invalid_answer is None:
             continue
         if not invalid_answer:
-            return False
+            return israel_entrance, hebrew_speaker
 
 
 def under_eighteen(birthday_date):
@@ -104,6 +103,7 @@ def under_eighteen(birthday_date):
 
 def knows_candidates():
     while True:
+        candidates_names = []
         know_candidates = input("Do you know any candidates (yes/no): ")
         invalid_answer = check_yes_no(know_candidates)
         if invalid_answer is None:
@@ -111,10 +111,11 @@ def knows_candidates():
         if not invalid_answer:
             if know_candidates == "yes":
                 candidate_name = input("Who do you know: ")
+                candidates_names.append(candidate_name)
                 continue
             else:
                 print("Well done. Your registration is complete.")
-                break
+                return candidates_names
 
 
 main()
