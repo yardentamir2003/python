429cd4da0aff393db5d9431be96eaf7056795d43
yarden <yardent9@gmail.com>
2022-04-04 14:30:34 +0300

    cotere

--- basics/candidates_files/try.py
+++ basics/candidates_files/try.py
@@ -4,30 +4,24 @@ import re
 
 def main():
     print("Welcome candidate!\nPlease enter the following details:")
-    #files_amount = 0o1
-    #for file in files:
-        #if files_amount > 100:
-            #print("candidates list is full, try next year.")
-            #exit(0)
-        #file_name = "cadidate_{}.json".format(files_amount)
+    # files_amount = 0o1
+    # for file in files:
+    # if files_amount > 100:
+    # print("candidates list is full, try next year.")
+    # exit(0)
+    # file_name = "cadidate_{}.json".format(files_amount)
     d = ask_questions()
-        #output_file = open(file_name, "w")
-        #json.dump(d, output_file, indent=6)
-        #files_amount += 1
+    # output_file = open(file_name, "w")
+    # json.dump(d, output_file, indent=6)
+    # files_amount += 1
 
 
 def ask_questions():
     d = {}
     d["first_name"] = input("What is your first name: ")
     d["last_name"] = input("What is your last name: ")
-    while True:
-        birth_date = input("What is your birth date: ")
-        invalid_date = check_date(birth_date)
-        if invalid_date is None:
-            continue
-        if not invalid_date:
-            break
-    d["birth day"] = birth_date
+    birth_date = get_date("Enter your birth date: ")
+    d["birth date"] = birth_date
     parent_approval, school_name, grade = under_eighteen(birth_date)
     birth_country = input("What is your birth country: ")
     if birth_country != "israel" and birth_country != "Israel":
@@ -47,72 +41,64 @@ def ask_questions():
     return d
 
 
-def check_date(date):
+def get_date(question):
+    while True:
+        date = input(question)
+        if is_date_valid(date):
+            return date
+        print("Ahhh.. Are you sure? Please enter date in format yyyy-mm-dd")
+
+
+def is_date_valid(date):
     match_date = re.findall('^[0-9]{4}-[0-9]{2}-[0-9]{2}$', date)
     if len(match_date) == 0:
-        print("Ahhh.. Are you sure? Please enter date in format yyyy-mm-dd")
-        return None
-    else:
         return False
+    return True
 
 
-def check_yes_no(question):
-    match_answer = re.findall("^(?:yes|no)$", question)
-    if len(match_answer) == 0:
+def get_answer(question):
+    while True:
+        answer = input(question)
+        if check_yes_no(answer):
+            return answer
         print("Ahhh.. Are you sure? Please enter yes or no")
-        return None
-    else:
+
+
+def check_yes_no(answer):
+    match_answer = re.findall("^(?:yes|no)$", answer)
+    if len(match_answer) == 0:
         return False
+    return True
 
 
 def non_israel_function():
-    while True:
-        israel_entrance = input("When did you enter to Israel: ")
-        invalid_date = check_date(israel_entrance)
-        if invalid_date is None:
-            continue
-        if not invalid_date:
-            break
-    while True:
-        hebrew_speaker = input("Do you speak hebrew (yes/no): ")
-        invalid_answer = check_yes_no(hebrew_speaker)
-        if invalid_answer is None:
-            continue
-        if not invalid_answer:
-            return israel_entrance, hebrew_speaker
+    israel_entrance = get_date("When did you enter to Israel: ")
+    hebrew_speaker = get_answer("Do you speak hebrew (yes/no): ")
+    return israel_entrance, hebrew_speaker
 
 
 def under_eighteen(birthday_date):
     year = birthday_date[:4]
     year = int(year)
     if year > 2004:
-        while True:
-            parent_approval = input("Do we have parents approval (yes/no): ")
-            invalid_answer = check_yes_no(parent_approval)
-            if invalid_answer is None:
-                continue
-            if not invalid_answer:
-                break
+        parent_approval = get_answer("Do we have parents approval (yes/no): ")
         school_name = input("what is your school name: ")
         grade = input("What is your grade: ")
         return parent_approval, school_name, grade
 
 
 def knows_candidates():
+    candidates_names = []
     while True:
-        candidates_names = []
-        know_candidates = input("Do you know any candidates (yes/no): ")
-        invalid_answer = check_yes_no(know_candidates)
-        if invalid_answer is None:
-            continue
-        if not invalid_answer:
-            if know_candidates == "yes":
-                candidate_name = input("Who do you know: ")
-                candidates_names.append(candidate_name)
-                continue
-            else:
-                print("Well done. Your registration is complete.")
-                return candidates_names
+        knows_candidates = get_answer("Do you know any candidates (yes/no): ")
+        if knows_candidates == "yes":
+            candidate_name = input("Who do you know: ")
+            candidates_names.append(candidate_name)
+                    continue
+            print("Well done. Your registration is complete.")
+            return candidates_names
+
+
 
 
 main()
