12affae5ae7bde385611e1d3af3f990dcfdcd8a4
yarden tamir <yardent9@gmail.com>
2022-04-20 16:39:02 +0300

    commit

--- exercises/math_exercises/main.py
+++ exercises/math_exercises/main.py
@@ -3,23 +3,21 @@ import re
 
 def main():
     while True:
-        option_number = input("1. Solve exercises\n2. Check exercises\n3. Quit\nchoose a number: ")
-        valid_option_number = check_option_number(option_number)
-        if valid_option_number:
-            if option_number == "3":
-                break
-            if option_number == "1":
-                ...
-            if option_number == "2":
-                ...
+        option_number = get_option_number()
+        if option_number == "3":
+            break
+        if option_number == "1":
+            ...
+        if option_number == "2":
+            ...
 
 
 def get_option_number():
     while True:
-        option_number = input("choose a number:\n1. Solve exercises\n2. Check exercises\n3. Quit")
+        option_number = input("1. Solve exercises\n2. Check exercises\n3. Quit\nchoose a number: ")
         valid_option_number = check_option_number(option_number)
         if valid_option_number:
-            return valid_option_number
+            return option_number
         print("please enter a number from 1 to 3.")
 
 
@@ -30,7 +28,24 @@ def check_option_number(option_number):
     return True
 
 
-def
+def solve_exercises():
+    exercises = get_exercises()
+    with open("output.txt", "a") as file:
+
+        file.write(answer)
+
+
+def get_exercises():
+    exercises = []
+    with open("input.txt", "r") as file:
+        for line in file:
+            line.strip()
+            exercises.append(line)
+        return exercises
+
+# def check_exercises():
+#     with open
+#
 
 
 if __name__ == '__main__':
