477e1f90e6e15445e70197587e65846812972121
yarden tamir <yardent9@gmail.com>
2022-04-29 17:53:45 +0300

    commit

--- exercises/math_exercises/test.py
+++ exercises/math_exercises/test.py
@@ -2,4 +2,13 @@ from main import valid_exercise
 
 
 def test_valid_exercise():
-    valid =
+    valid = valid_exercise("1+2+6")
+    if valid_exercise:
+        print("should be not valid.")
+
+    valid = valid_exercise("309-80")
+    if not valid:
+        print("should be valid")
+
+
+test_valid_exercise()
