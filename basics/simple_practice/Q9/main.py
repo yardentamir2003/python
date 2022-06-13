7af884b53936626e90ff70eab7e62bea962aa1d2
yarden tamir <yardent9@gmail.com>
2022-06-13 21:03:20 +0300

    commit

--- basics/simple_practice/Q9/main.py
+++ basics/simple_practice/Q9/main.py
@@ -1,29 +1,28 @@
+import re
+
+
 def main():
     x_row = 0
     x_col = 0
     draw_board(0, 0)
     while True:
         move = input("Enter your move: ")
-
-            if x_row == 0 and move == "up" or x_row == 3 and move == "down" or x_col == 0 and move == "left" or x_col == 3 and move == "right":
-                print("Error: unable to move {}.".format(move))
-                continue
-            if move == "up":
-                x_row -= 1
-            if move == "down":
-                x_row += 1
-            if move == "right":
-                x_col += 1
-            if move == "left":
-                x_col -= 1
-
-            if move == "quit":
-                print("Bye bye, have a nice day.")
-                break
-            draw_board(x_row, x_col)
-            else:
-                print("Please enter: up, left, right, down, or quit.")
-                draw_board(0, 0)
+        if valid_input(move):
+            if valid_move(move, x_row, x_col):
+                if move == "up":
+                    x_row -= 1
+                if move == "down":
+                    x_row += 1
+                if move == "right":
+                    x_col += 1
+                if move == "left":
+                    x_col -= 1
+                if move == "quit":
+                    print("Bye bye, have a nice day.")
+                    break
+                draw_board(x_row, x_col)
+        else:
+            print("Please enter: up, left, right, down, or quit.")
 
 
 def draw_board(x_row, x_col):
@@ -34,6 +33,20 @@ def draw_board(x_row, x_col):
             else:
                 print("o", end="")
         print()
+
+
+def valid_input(move):
+    match_input = re.findall('^(?:up|down|right|left|quit)$', move)
+    if len(match_input) == 0:
+        return False
+    return True
+
+
+def valid_move(move, x_row, x_col):
+    if x_row == 0 and move == "up" or x_row == 3 and move == "down" or x_col == 0 and move == "left" or x_col == 3 and move == "right":
+        print("Error: unable to move {}.".format(move))
+        return False
+    return True
 
 
 main()
