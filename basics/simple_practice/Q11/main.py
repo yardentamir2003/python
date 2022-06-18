768b335e9d97daa90b1eeac6323a50c8f220c9b7
Yarden <yardent9@gmail.com>
2022-06-18 17:44:43 +0300

    test

---
+++ basics/simple_practice/Q11/main.py
@@ -0,0 +1,38 @@
+import re
+
+
+def main():
+    draw_board()
+    while True:
+        board_location = input("Enter the queen target destination: ")
+        if valid_board_location(board_location):
+            if valid_move(board_location):
+                break
+            print("{} is not a valid move.".format(board_location))
+        print("{} is not a valid board location.".format(board_location))
+
+
+def draw_board():
+    row_numbers = ["8", "7", "6", "5", "4", "3", "2", "1", ""]
+    col_numbers = [" ", "A", "B", "C", "D", "E", "F", "G", "H"]
+    for row in range(9):
+        for col in range(9):
+            if col == 0:
+                print(row_numbers[row], end="")
+            if row == 8:
+                print(col_numbers[col], end="")
+            else:
+                print("o", end="")
+        print()
+
+
+def valid_board_location(board_location):
+    match = re.findall('^[A-H][1-8]$', board_location)
+    if len(match) == 0:
+        return False
+    return True
+
+
+def valid_move(board_location):
+
+draw_board()
---
+++ basics/simple_practice/Q11/try.py
@@ -0,0 +1,15 @@
+def draw_board():
+    row_numbers = ["8", "7", "6", "5", "4", "3", "2", "1", ""]
+    col_numbers = [" ", "A", "B", "C", "D", "E", "F", "G", "H"]
+    for row in range(9):
+        for col in range(9):
+            if col == 0:
+                print(row_numbers[row], end="")
+            if row == 8:
+                print(col_numbers[col], end="")
+            else:
+                print("o", end="")
+        print()
+
+
+draw_board()
