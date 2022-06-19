36291de220462313dcb5d52492629c5ab6aa6e7f
yarden tamir <yardent9@gmail.com>
2022-06-19 22:59:37 +0300

    commit

--- basics/simple_practice/Q11/try.py
+++ basics/simple_practice/Q11/try.py
@@ -1,10 +1,11 @@
 def draw_board():
-    row_numbers = ["8", "7", "6", "5", "4", "3", "2", "1", ""]
+    row_numbers = ["8", "7", "6", "5", "4", "3", "2", "1", " "]
     col_numbers = [" ", "A", "B", "C", "D", "E", "F", "G", "H"]
     for row in range(9):
         for col in range(9):
             if col == 0:
                 print(row_numbers[row], end="")
+                continue
             if row == 8:
                 print(col_numbers[col], end="")
             else:
