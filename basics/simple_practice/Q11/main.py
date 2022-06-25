80700d7eca3dc1195c40d8c7af4784776e8c1a22
yarden tamir <yardent9@gmail.com>
2022-06-25 18:07:56 +0300

    commit

--- basics/simple_practice/Q11/main.py
+++ basics/simple_practice/Q11/main.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 
 def main():
@@ -10,27 +11,25 @@ def main():
         if not valid_board_location(board_location):
             print("{} is not a valid board location.".format(board_location))
             continue
-        target_row,target_col = parse_location(board_location)
-        if not valid_move(row_queen, col_queen,target_row,target_col):
+        target_row, target_col = parse_location(board_location)
+        if not valid_move(row_queen, col_queen, target_row, target_col):
             print("{} is not a valid move.".format(board_location))
             continue
 
-        row_queen,col_queen=target_row,target_col
-
-        while col_queen<target_col:
-            col_queen+=1
+        while col_queen < target_col:
+            col_queen += 1
             draw_board(row_queen, col_queen)
-        # loop col_queen<target_col:
-        #     +1
-        #     draw_board(row_queen, col_queen)
-
+            if col_queen == target_col:
+                print("The queen rests.")
+            else:
+                print("The queen is still moving…")
+                time.sleep(1)
 
 
 def draw_board(row_queen, col_queen):
     row_numbers = [" ", "1", "2", "3", "4", "5", "6", "7", "8"]
     col_numbers = [" ", "A", "B", "C", "D", "E", "F", "G", "H"]
     for row in range(8, -1, -1):
-        # print("row:", row)
         for col in range(9):
             if col == col_queen and row == row_queen:
                 print("x", end="")
@@ -52,16 +51,18 @@ def valid_board_location(board_location):
     return True
 
 
-def valid_move(row_queen, col_queen,target_row,target_col):
-    # move right
-    if target_col
+def valid_move(row_queen, col_queen, target_row, target_col):
+    # moves right
+    if target_col > col_queen and target_row == row_queen:
+        return True
+    return False
 
 
 def parse_location(board_location):
-    letter_to_col = {"A":1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":7, "H":8}
+    letter_to_col = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8}
     letter = board_location[0]
     target_col = letter_to_col[letter]
-    target_row = board_location[1]
+    target_row = int(board_location[1])
     return target_row, target_col
 
 
