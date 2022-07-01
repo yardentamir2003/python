57ba8f23059187141dd0d5f395cb5fff8c6a3eb6
yarden tamir <yardent9@gmail.com>
2022-07-01 16:58:03 +0300

    commit

--- basics/simple_practice/Q11/try.py
+++ basics/simple_practice/Q11/try.py
@@ -2,6 +2,52 @@ import re
 import time
 
 
+def parse_location(board_location):
+    letter_to_col = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8}
+    letter = board_location[0]
+    target_col = letter_to_col[letter]
+    target_row = int(board_location[1])
+    return target_row, target_col
+
+
+def valid_move(row_queen, col_queen, target_row, target_col):
+    if target_col > col_queen and target_row == row_queen:
+        return "right"
+    elif target_col < col_queen and target_row == row_queen:
+        return "left"
+    elif target_col == col_queen and target_row > row_queen:
+        return "up"
+    elif target_col == col_queen and target_row < row_queen:
+        return "down"
+    else:
+        return False
+
+
+def valid_board_location(board_location):
+    match = re.findall('^[A-H][1-8]$', board_location)
+    if len(match) == 0:
+        return False
+    return True
+
+
+def draw_board(row_queen, col_queen):
+    row_numbers = [" ", "1", "2", "3", "4", "5", "6", "7", "8"]
+    col_numbers = [" ", "A", "B", "C", "D", "E", "F", "G", "H"]
+    for row in range(8, -1, -1):
+        for col in range(9):
+            if col == col_queen and row == row_queen:
+                print("x", end="")
+                continue
+            if col == 0:
+                print(row_numbers[row], end="")
+                continue
+            if row == 0:
+                print(col_numbers[col], end="")
+            else:
+                print("o", end="")
+        print()
+
+
 def move_right(row_queen, col_queen, target_col):
     while col_queen < target_col:
         col_queen += 1
@@ -76,52 +122,6 @@ def main():
             row_queen = move_up(row_queen, col_queen, target_row)
         elif move == "down":
             row_queen = move_down(row_queen, col_queen, target_row)
-
-
-def draw_board(row_queen, col_queen):
-    row_numbers = [" ", "1", "2", "3", "4", "5", "6", "7", "8"]
-    col_numbers = [" ", "A", "B", "C", "D", "E", "F", "G", "H"]
-    for row in range(8, -1, -1):
-        for col in range(9):
-            if col == col_queen and row == row_queen:
-                print("x", end="")
-                continue
-            if col == 0:
-                print(row_numbers[row], end="")
-                continue
-            if row == 0:
-                print(col_numbers[col], end="")
-            else:
-                print("o", end="")
-        print()
-
-
-def valid_board_location(board_location):
-    match = re.findall('^[A-H][1-8]$', board_location)
-    if len(match) == 0:
-        return False
-    return True
-
-
-def valid_move(row_queen, col_queen, target_row, target_col):
-    if target_col > col_queen and target_row == row_queen:
-        return "right"
-    elif target_col < col_queen and target_row == row_queen:
-        return "left"
-    elif target_col == col_queen and target_row > row_queen:
-        return "up"
-    elif target_col == col_queen and target_row < row_queen:
-        return "down"
-    else:
-        return False
-
-
-def parse_location(board_location):
-    letter_to_col = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8}
-    letter = board_location[0]
-    target_col = letter_to_col[letter]
-    target_row = int(board_location[1])
-    return target_row, target_col
 
 
 main()
