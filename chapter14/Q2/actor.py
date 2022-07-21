b8304369a306192a60261b9465ca657a926de42c
yarden tamir <yardent9@gmail.com>
2022-07-21 17:08:19 +0300

    commit

--- chapter14/Q2/actor.py
+++ chapter14/Q2/actor.py
@@ -1,6 +1,15 @@
+import datetime
+
 class Actor:
     def __init__(self, name, birth_year, movies):
         self.name = name
-        self.age =
+        self.birth_year = birth_year
+        self.movies = movies
+
     def is_in_age_range(self, from_age,to_age):
-    def is_in_movie(self, move_name):
+        birth_year = int(self.birth_year)
+        age = datetime.datetime.now().year - birth_year
+        print(age)
+
+
+    # def is_in_movie(self, move_name):
--- chapter14/Q2/main.py
+++ chapter14/Q2/main.py
@@ -1,16 +1,17 @@
-import datetime
 import re
 from actor import Actor
 
 
 def main():
+    actors_list = []
     while True:
         option = input("1. List actors by age\n2. Add actor\n3. Delete actor\n4. Delete movie\n5. Quit\nChoose option: ")
         if valid_option(option):
             # if option == "1":
             #     option_one()
             if option == "2":
-                option_two()
+                option_two(actors_list)
+                break
             # if option == "3":
             #     option_three()
             # if option == "4":
@@ -19,7 +20,7 @@ def main():
                 print("OK, have a good one!")
                 break
             else:
-                print("Invalid input, please enter 1/2/3/4")
+                print("Invalid input, please enter 1/2/3/4/5")
 
 
 def valid_option(option):
@@ -29,21 +30,27 @@ def valid_option(option):
     return True
 
 
-def option_two():
+def option_two(actors_list):
     actor_name = input("Enter the actor’s name: ")
     while True:
         actor_birth_year = input("Enter the actor’s birth year: ")
         if valid_birth_year(actor_birth_year):
             break
-    actor_age = calculate_age(actor_birth_year)
-    actor = Actor(actor_name, actor_age)
+    movies = []
+    movie_count = 0
+    while True:
+        movie = input("Enter the actor’s movies, or click Enter to complete: ")
+        if movie == "":
+            actor = Actor(actor_name, actor_birth_year, movies)
+            print("OK, actor {} added, and {} movies specified.".format(actor_name, movie_count))
+            break
+        else:
+            movies.append(movie)
+            movie_count += 1
 
+    actors_list.append(actor)
+    # actor.is_in_age_range()
 
-def calculate_age(actor_birth_year):
-    birth_year = int(actor_birth_year)
-    age = datetime.datetime.now().year - birth_year
-    print(age)
-    return age
 
 
 def valid_birth_year(actor_birth_year):
