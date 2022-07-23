7d47484db9027a3c451822ed83fcea7b51ff4bf5
yarden tamir <yardent9@gmail.com>
2022-07-23 12:12:42 +0300

    commit

--- chapter14/Q2/main.py
+++ chapter14/Q2/main.py
@@ -1,5 +1,4 @@
 import re
-from actor import Actor
 
 
 def main():
@@ -7,8 +6,8 @@ def main():
     while True:
         option = input("1. List actors by age\n2. Add actor\n3. Delete actor\n4. Delete movie\n5. Quit\nChoose option: ")
         if valid_option(option):
-            # if option == "1":
-            #     option_one()
+            if option == "1":
+                option_one(actors_list)
             if option == "2":
                 option_two(actors_list)
                 continue
@@ -31,12 +30,30 @@ def valid_option(option):
     return True
 
 
+def valid_age_range(age_range):
+    match = re.findall('^(\d{1,3}-\d{1,3})$', age_range)
+    if len(match) == 0:
+        return False
+    return True
+
+
+def option_one(actors_list):
+    while True:
+        age_range = input("Enter actors age range: ")
+        if valid_age_range(age_range):
+            break
+        else:
+            print("Please enter age range in format Ddd-Ddd, when d is optional.")
+
+
 def option_two(actors_list):
     actor_name = input("Enter the actor’s name: ")
     while True:
         actor_birth_year = input("Enter the actor’s birth year: ")
         if valid_birth_year(actor_birth_year):
             break
+        else:
+            print("Please enter a year in format DDDD.")
     movies = []
     movie_count = 0
     while True:
@@ -56,13 +73,13 @@ def option_two(actors_list):
 def option_three(actors_list):
     while True:
         deleted_actor = input("Enter actor name: ")
-        if deleted_actor in actors_list:
+        # if deleted_actor in actors_list:
+        if any(obj['actor'] == deleted_actor for obj in actors_list):
+            actors_list.remove(deleted_actor)
             break
         else:
             print("{} doesn't exist in the actors list.".format(deleted_actor))
     print("OK, the actor {} was deleted".format(deleted_actor))
-
-
 
 
 def valid_birth_year(actor_birth_year):
@@ -70,5 +87,6 @@ def valid_birth_year(actor_birth_year):
     if len(match) == 0:
         return False
     return True
+
 
 main()
