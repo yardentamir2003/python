ee27709942d1eada5fda744e419c14c8c214f2ac
yarden tamir <yardent9@gmail.com>
2022-07-29 15:44:44 +0300

    commit

--- chapter14/Q3/actors.py
+++ chapter14/Q3/actors.py
@@ -1,15 +1,103 @@
+import re
+
+from chapter14.Q3.actor import Actor
+
+
 class Actors:
     def __init__(self):
-        pass
+        self.actors_list = []
 
     def list_by_age(self):
-        pass
+        if len(self.actors_list) == 0:
+            print("Actors list is empty.")
+            return
+        while True:
+            age_range = input("Enter actors age range: ")
+            if valid_age_range(age_range):
+                break
+            else:
+                print("Please enter age range in format Ddd-Ddd, when d is optional.")
+        splitted_age_range = age_range.split("-")
+        from_age = int(splitted_age_range[0])
+        to_age = int(splitted_age_range[1])
+        zero_match = True
+        for actor in self.actors_list:
+            if actor.is_in_age_range(from_age, to_age):
+                zero_match = False
+                movies_number = len(actor.movies)
+                print("{}, ({} movies).".format(actor.name, movies_number))
+        if zero_match:
+            print("Can't find actors in this age range.")
 
     def add_actor(self):
-        pass
+        actor_name = input("Enter the actor’s name: ")
+        for actor in self.actors_list:
+            if actor_name == actor.name:
+                print("The actor {} already exist.".format(actor_name))
+                return
+        while True:
+            actor_birth_year = input("Enter the actor’s birth year: ")
+            if valid_birth_year(actor_birth_year):
+                break
+            else:
+                print("Please enter a year in format DDDD.")
+        movies = []
+        movie_count = 0
+        while True:
+            movie = input("Enter the actor’s movies, or click Enter to complete: ")
+            if movie == "":
+                actor = Actor(actor_name, actor_birth_year, movies)
+                print("OK, actor {} added, and {} movies specified.".format(actor_name, movie_count))
+                break
+            else:
+                movies.append(movie)
+                movie_count += 1
+
+        self.actors_list.append(actor)
 
     def delete_actor(self):
-        pass
+        while True:
+            if len(self.actors_list) == 0:
+                print("The actors list is empty. Can't delete actors.")
+                return
+            deleted_actor = input("Enter actor name: ")
+            for actor in self.actors_list:
+                if deleted_actor == actor.name:
+                    self.actors_list.remove(actor)
+                    print("OK, the actor {} was deleted".format(deleted_actor))
+                    return
+            print("{} doesn't exist in the actors list.".format(deleted_actor))
 
     def delete_movie(self):
-        pass
+        if len(self.actors_list) == 0:
+            print("Actors list is empty.")
+            return
+        movie_name = input("Enter movie name: ")
+        affected_actors = 0
+        for actor in self.actors_list:
+            if actor.is_in_movie(movie_name):
+                affected_actors += 1
+                actor.movies.remove(movie_name)
+        if affected_actors != 0 and affected_actors != 1:
+            print(
+                "OK, the movie {} was deleted, {} actors were affected by this change.".format(movie_name,
+                                                                                               affected_actors))
+        elif affected_actors == 0:
+            print("The movie doesn't exist.")
+        else:
+            print(
+                "OK, the movie {} was deleted, 1 actor was affected by this change.".format(movie_name))
+
+
+def valid_age_range(age_range):
+    match = re.findall('^(\d{1,3}-\d{1,3})$', age_range)
+    if len(match) == 0:
+        return False
+    return True
+
+
+def valid_birth_year(actor_birth_year):
+    match = re.findall('^\d{4}$', actor_birth_year)
+    if len(match) == 0:
+        return False
+    return True
--- chapter14/Q3/main.py
+++ chapter14/Q3/main.py
@@ -1,24 +1,25 @@
 import re
-from chapter14.Q2.actor import Actor
+from chapter14.Q3 import actors
+from chapter14.Q3.actors import Actors
 
 
 def main():
-    actors_list = []
+    actors_list = Actors()
     while True:
         option = input(
             "1. List actors by age\n2. Add actor\n3. Delete actor\n4. Delete movie\n5. Quit\nChoose option: ")
         if valid_option(option):
             if option == "1":
-                option_one(actors_list)
+                actors.list_by_age()
                 continue
             if option == "2":
-                option_two(actors_list)
+                actors.add_actor()
                 continue
             if option == "3":
-                option_three(actors_list)
+                actors.delete_actor()
                 continue
             if option == "4":
-                option_four(actors_list)
+                actors.delete_movie()
                 continue
             if option == "5":
                 print("OK, have a good one!")
@@ -32,104 +33,6 @@ def valid_option(option):
     if len(match) == 0:
         return False
     return True
-
-
-def valid_age_range(age_range):
-    match = re.findall('^(\d{1,3}-\d{1,3})$', age_range)
-    if len(match) == 0:
-        return False
-    return True
-
-
-def valid_birth_year(actor_birth_year):
-    match = re.findall('^\d{4}$', actor_birth_year)
-    if len(match) == 0:
-        return False
-    return True
-
-
-def option_one(actors_list):
-    if len(actors_list) == 0:
-        print("Actors list is empty.")
-        return
-    while True:
-        age_range = input("Enter actors age range: ")
-        if valid_age_range(age_range):
-            break
-        else:
-            print("Please enter age range in format Ddd-Ddd, when d is optional.")
-    splitted_age_range = age_range.split("-")
-    from_age = int(splitted_age_range[0])
-    to_age = int(splitted_age_range[1])
-    zero_match = True
-    for actor in actors_list:
-        if actor.is_in_age_range(from_age, to_age):
-            zero_match = False
-            movies_number = len(actor.movies)
-            print("{}, ({} movies).".format(actor.name, movies_number))
-    if zero_match:
-        print("Can't find actors in this age range.")
-
-
-def option_two(actors_list):
-    actor_name = input("Enter the actor’s name: ")
-    for actor in actors_list:
-        if actor_name == actor.name:
-            print("The actor {} already exist.".format(actor_name))
-            return
-    while True:
-        actor_birth_year = input("Enter the actor’s birth year: ")
-        if valid_birth_year(actor_birth_year):
-            break
-        else:
-            print("Please enter a year in format DDDD.")
-    movies = []
-    movie_count = 0
-    while True:
-        movie = input("Enter the actor’s movies, or click Enter to complete: ")
-        if movie == "":
-            actor = Actor(actor_name, actor_birth_year, movies)
-            print("OK, actor {} added, and {} movies specified.".format(actor_name, movie_count))
-            break
-        else:
-            movies.append(movie)
-            movie_count += 1
-
-    actors_list.append(actor)
-
-
-def option_three(actors_list):
-    while True:
-        if len(actors_list) == 0:
-            print("The actors list is empty. Can't delete actors.")
-            return
-        deleted_actor = input("Enter actor name: ")
-        for actor in actors_list:
-            if deleted_actor == actor.name:
-                actors_list.remove(actor)
-                print("OK, the actor {} was deleted".format(deleted_actor))
-                return
-        print("{} doesn't exist in the actors list.".format(deleted_actor))
-
-
-def option_four(actors_list):
-    if len(actors_list) == 0:
-        print("Actors list is empty.")
-        return
-    movie_name = input("Enter movie name: ")
-    affected_actors = 0
-    for actor in actors_list:
-        if actor.is_in_movie(movie_name):
-            affected_actors += 1
-            actor.movies.remove(movie_name)
-    if affected_actors != 0 and affected_actors != 1:
-        print(
-            "OK, the movie {} was deleted, {} actors were affected by this change.".format(movie_name, affected_actors))
-    elif affected_actors == 0:
-        print("The movie doesn't exist.")
-    else:
-        print(
-            "OK, the movie {} was deleted, 1 actor was affected by this change.".format(movie_name))
 
 
 main()
