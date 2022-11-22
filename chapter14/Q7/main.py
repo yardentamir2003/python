7ea5dc58e01ffd55c3eda4c560be17e4c2daedf0
yarden tamir <yardent9@gmail.com>
2022-11-22 22:07:09 +0200

    commit

--- chapter14/Q7/main.py
+++ chapter14/Q7/main.py
@@ -1,24 +1,30 @@
 import re
 from chapter14.Q7.library import Library
+from chapter14.Q7.menu import Menu
 
 
 def main():
     library = Library()
     library.load_all_from_files()
-    while True:
-        number = input(
-            "What would you like to manage:\n1. Books Inventory Management\n2. System Readers Management System\n"
-            "3. quit\n")
-        if valid_number(number):
-            if number == "1":
-                library.show_books_menu()
-            elif number == "2":
-                library.show_readers_menu()
-            else:
-                print("Ok, have a good one!")
-                return
-        else:
-            print("Invalid input, please enter 1/2/3.")
+    menu = Menu()
+    menu.add_option("Books Inventory Management", library.show_books_menu)
+    menu.add_option("System Readers Management System", library.show_readers_menu)
+    menu.show_menu()
+
+    # while True:
+    #     number = input(
+    #         "What would you like to manage:\n1. Books Inventory Management\n2. System Readers Management System\n"
+    #         "3. quit\n")
+    #     if valid_number(number):
+    #         if number == "1":
+    #             library.show_books_menu()
+    #         elif number == "2":
+    #             library.show_readers_menu()
+    #         else:
+    #             print("Ok, have a good one!")
+    #             return
+    #     else:
+    #         print("Invalid input, please enter 1/2/3.")
 
 
 def valid_number(number):
@@ -28,8 +34,7 @@ def valid_number(number):
     return True
 
 
-def print_blessing():
-    print("Have a good day!")
+
 
 
 main()
--- chapter14/Q7/menu.py
+++ chapter14/Q7/menu.py
@@ -1,12 +1,47 @@
+import re
+
+
 class Menu:
     def __init__(self):
-        pass
+        self.options = []
 
     def add_option(self, option_name, callback_function):
-        pass
+        option_function = (option_name, callback_function)
+        self.options.append(option_function)
 
     def show_menu(self):
-        pass
+        while True:
+            index = 1
+            for option in self.options:
+                print("{}. {}".format(index, option[0]))
+                index += 1
+            print("{}. quit".format(len(self.options) + 1))
+            chosen_option = input("Select option: ")
+            chosen_option_index = int(chosen_option)
+            if index == len(self.options) + 1:
+                break
+            else:
+                chosen_function = self.options[chosen_option_index - 1][1]
+                chosen_function()
 
 
+def foo():
+    print("1")
 
+
+def foo2():
+    print("2")
+
+
+def valid_option(option):
+    match = re.findall("^[123]$", option)
+    if len(match) == 0:
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    menu = Menu()
+    menu.add_option("foo", foo)
+    menu.add_option("yarden", foo2)
+    menu.show_menu()
